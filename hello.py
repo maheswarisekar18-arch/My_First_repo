3e78eb493ac0884b3e9aea59f8aada31bb14361c
maheswari <maheswarisekar18@gmail.com>
2026-02-12 21:11:52 +0530

    calc code to commit changes to hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,20 @@
-print'("Hello,Git!")' 
+print("Hello,Git!")
 
-Print ("change")
+print ("change")
 
 print ("change2")
+
+def add(a,b):
+    return a+b
+print(add(1,2))
+
+def sub(a,b):
+    return a-b  
+print(sub(1,2))
+
+def mul(a,b):
+    return a*b
+print(mul(1,2))
+def div(a,b):
+    return a/b
+print(div(1,2))
